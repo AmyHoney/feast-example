8c7e5dfface756845badffbd0a17fa04cc9d3bda
ubuntu <vmware@sc1-10-168-207-161.nimbus.eng.vmware.com>
2023-02-21 07:28:13 +0000

    Delete unused files

--- 01_driver_ranking_feature_repo_local/infra/gcp/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_name" {
-  type        = string
-  description = "The project identifier is used to uniquely namespace resources"
-}
-
-variable "gcp_project" {
-  type        = string
-  description = "The GCP project id"
-}
